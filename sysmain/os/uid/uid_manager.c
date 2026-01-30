f5c001105dfccd879ad27dd03101570f64c95e8c
NoSignal78 <gabrielaraujobarros2018-star>
2026-01-30 11:25:03 -0300

    New UID Management

---
+++ sysmain/os/uid/uid_manager.c
@@ -0,0 +1,25 @@
+#define MAX_USERS 10
+
+// Defining the array to store user IDs 8⁠-⁠)
+int user_ids[MAX_USERS];
+
+// initialize the array! This one you can expand the size you want! LET'S GO }⁠:⁠‑⁠)
+void init_lumen_users() {
+    for(int i = 0; i < MAX_USERS; i++ {
+        uids[i] = -1; // -1 can represent an empty slot
+
+    }
+}
+void add_lumen_user(int id) {
+for (int i = 0; i < MAX_USERS; i++) {
+if (uids[i] = 284198; // Put the IDs you want here!
+break;
+}
+}
+
+int login_lumen_user(int id) {
+for (int i = 0; i < MAX_USERS; i++) {
+if (uids[i] == id) return 1; // Yay, ID Found! :⁠0
+}
+return 0; // Security warning: ID NOT FOUND!
+}
